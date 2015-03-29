c91c896a1a93c9aa18ae4b9deeea2d0323aadb97
Zamzee <randlauer@gmail.com>
2015-03-29 13:52:04 -0600

    Commit 40 3-29-15 CS-2300 & CS-2420 Folder Updates

---
+++ 1_ CS-2420-601/Projects/Project 5/CS-2420_Program_5_RCA_V1.0/CS-2420_Program_5_RCA_V1.0/Driver.cpp
@@ -0,0 +1,62 @@
+// File Prologue
+// Name: Russell Andlauer
+// Class: CS-2420-601
+// Program 5: Hashing
+// Date Last Modified: March 27th, 2015
+// Description: This program offers practice implementing a hash table and a hash function using a file containing a set of integers.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "Hash.h"
+using namespace std;
+
+int main()
+{
+
+	// Program Introduction
+	cout << "Name: Russell Andlauer\nClass: CS-2420-601\nProgram 5: Hashing\n";
+	cout << "Description:\n";
+	cout << "==============================================================\n";
+
+	// Start of Program
+
+	// Create Variables to read in data
+	ifstream inputFile;
+	string fileName;
+	int data;
+
+	//  Prompt User to enter the name of the file to be read from
+	cout << "\nPlease enter the name of the data file: ";
+	cin >> fileName;
+
+	// Open file, check for read error
+	inputFile.open(fileName);
+	if (inputFile.fail())
+	{
+		cout << "Error opening " << fileName << endl;
+		system("pause");
+		return 1;
+	}
+
+	// Print data from file
+
+	cout << "\nContents of the Data File" << endl;
+
+	while (!inputFile.eof())
+	{
+		inputFile >> data;
+
+		cout << data << endl;
+	}
+
+	// Close file
+	inputFile.close();
+
+	cout << "Program Complete" << endl;
+
+
+	system("pause");
+	return 0;
+}
---
+++ 1_ CS-2420-601/Projects/Project 5/CS-2420_Program_5_RCA_V1.0/CS-2420_Program_5_RCA_V1.0/Hash.cpp
@@ -0,0 +1,23 @@
+// File Prologue
+// Name: Russell Andlauer
+// Class: CS-2420-601
+// Program 5: Hashing
+// Date Last Modified: March 27th, 2015
+// Description: This program offers practice implementing a hash table and a hash function using a file containing a set of integers.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Hash.h"
+
+
+Hash::Hash()
+{
+}
+
+
+
+
+
+
+//Hash::~Hash()
+//{
+//}
