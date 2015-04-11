48ef98072f941c190384a08816f63fd3d2850516
Zamzee <randlauer@gmail.com>
2015-04-11 14:49:49 -0700

    Commit 51 - (4/11/15) Sat. p.m. CS2420_Proj06_v1.01
    CS-2420 Pseudo-code for Project 06 Driver.cpp and misc folder changes

---
+++ 1_ CS-2420-601/1_Projects/Project 6/CS-2420_Project_06_RCA_V1.0/CS-2420_Project_06_RCA_V1.0/Driver.cpp
@@ -0,0 +1,44 @@
+//========================================================================================//
+// File Prologue
+
+	// Author: Russell C. Andlauer
+
+	// Class: CS-2420-601
+
+	// Program 6: Sorting
+
+	// Date Last Modified: April 11th, 2015
+							// Start time: (12:15 pm, 4/11/15)
+							// File Completion: (  )
+
+	// Description: This program offers practice implementing several sorting algorithms and 
+	//				determining how each Sort Method performs in comparison to each other
+//========================================================================================//
+
+
+
+// Start of Program
+
+// 1) Implement an Insertion Sort for each file provided
+
+// 2) Implement a Shell Sort for each file provided
+
+// 3) Implement a Quick Sort for each file provided
+
+
+// Run each Sort () Method for all three files provided and determine:
+
+	// 1) Run Time
+
+	// 2) Number of Comparisons for each execution of the particular Sort Method
+
+// name of the sort
+
+// file number
+
+// elapsed clock ticks for the sort
+
+// elapsed time in seconds
+
+// output file name of the sorted file. 
+
